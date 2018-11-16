eb60a2e225aaf0f77049a1dd4ea0353c1d5d7c9e
Yihsun Ethan Cheng <33135703+YihsunEthanCheng@users.noreply.github.com>
2018-11-15 22:39:52 -0800

    Added training agent

--- run_traffic_sign_classifier_LeNet5.py
+++ run_traffic_sign_classifier_LeNet5.py
@@ -17,14 +17,14 @@ Pool = {
         }
 C1 = {
        'ksize' : [5, 5],
-       'n': 6,
-       'strides': [1, 1, 1, 1],Tf
+       'n': 64,
+       'strides': [1, 1, 1, 1],
        'pool': Pool
        }
 
 C2 = {
        'ksize' : [5, 5],
-       'n': 16,
+       'n': 32,
        'strides': [1, 1, 1, 1],
        'pool': Pool
        }
@@ -36,47 +36,49 @@ params = {
         'sigma' :  0.1,
         'c1': C1, 
         'c2': C2,
-        'nFC1' : 120,
-        'nFC2' : 84,
-        'nCls' : 43,
-        'imsize' : [32, 32, 3],
+        'nFC1' : 256,
+        'nFC2' : 256,
         'lr' : 0.001,
         'BATCH_SIZE' : 256,
-        'EPOCHS' : 1
+        'EPOCHS' : 500
         } 
 
 #%%
 from modules.LeNet import LeNet
 import tensorflow as tf
 
-#def main():
+#%%def main():
     
-data = dataLoader(params['BATCH_SIZE'])
+data = dataLoader()
 params['nCls'] = data.nCls
 params['imsize'] = data.imsize
-X = tf.placeholder(tf.float32, tuple([None] + params['imsize']))
-Y = tf.placeholder(tf.int32, (None, params['nCls']))
+X = tf.placeholder(tf.float32, (None,) + params['imsize'])
+Y = tf.placeholder(tf.int32, (None))
 model = LeNet(X, Y, params)
-
-image = tf.placeholder(tf.float32, [None, 784])self.x_train.shape[1:3]
-label = tf.placeholder(tf.float32, [None, 10])
-model = Model(X, Y)
-saver = tf.train.saver()
+saver = tf.train.Saver()
 ii = np.arange(len(data.y_train))
-valid_accuracy = [0]
+valid_accuracy = []
+best_accuracy = 0
+#%%
+
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    for ep in range(param['EPOCHS']):
+    for ep in range(params['EPOCHS']):
         np.random.shuffle(ii)
-        for offset in range(0, len(data.x_train), BATCH_SIZE):
-            jj = ii[offset:offset + BATCH_SIZE]
+        for offset in range(0, len(data.x_train), params['BATCH_SIZE']):
+            jj = ii[offset:offset + params['BATCH_SIZE']]
             sess.run(model.optimize, {X: data.x_train[jj], Y: data.y_train[jj]})
-        valid_accuracy += [sess.run(model.error, {X: data.x_valid, Y: data.y_valid})]
-        print('Validation error {:6.2f}%'.format(100 * error))
-        if valid_accuracy[-1] > valid_accuracy[-2]:
+        valid_accuracy += [sess.run(model.accuracy, {X: data.x_valid, Y: data.y_valid})]
+        print('Validation error {:6.2f}%'.format(100 * valid_accuracy[-1]))
+        if valid_accuracy[-1] > best_accuracy:
             saver.save(sess, 'checkpoints/lenet5')
+            best_accuracy = valid_accuracy[-1]
+            print('best accuracy @ ep#{} = {}'.format(ep, best_accuracy))
 
-
+#%%
+with tf.Session() as sess:
+     test_accuracy = sess.run(model.accuracy, {X: data.x_test, Y: data.y_test})
+print('Test accuracy : {}'.format(test_accuracy))
 #if __name__ == '__main__':
 #    main()
 
