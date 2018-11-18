a3cca502e4d2b5710b2232299470ba1b2b21ff50
Yihsun Ethan Cheng <33135703+YihsunEthanCheng@users.noreply.github.com>
2018-11-18 00:18:47 -0800

    Added warpImage, imshow to dataLoader

--- modules/dataLoader.py
+++ modules/dataLoader.py
@@ -8,7 +8,6 @@ Created on Sat Nov 10 13:20:29 2018
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
-import skimage.transform as skt
 from skimage import color, exposure, transform
 
 class dataLoader(object):
@@ -26,7 +25,12 @@ class dataLoader(object):
         self.nCls = len(np.unique(train['labels']))
         self.imsize = train['features'].shape[1:]
         params.update(self.__dict__)
-       
+        
+        # import params
+        self.rotate_range = params['rotate_range']
+        self.translate_range = params['translate_range']
+        self.scaling_range = params['scaling_range']
+        
         # data stats
         self.y_train, self.y_valid, self.y_test = train['labels'], valid['labels'], test['labels']
         self.labs = np.unique(self.y_valid)
@@ -69,10 +73,7 @@ class dataLoader(object):
         mosaic = []
         idx = np.vstack([np.where(self.y_train == lab_i)[0][:nPerCls] for lab_i in self.labs])
         mosaic = np.vstack([np.hstack([ self.x_train[i] for i in row]) for row in idx])
-        if mosaic.dtype != np.uint8:
-            L, H = np.min(mosaic), np.max(mosaic)
-            mosaic = (mosaic - L) / (H - L )
-        plt.imshow(mosaic)
+        self.imshow(mosaic)
         return mosaic
             
     def processImage(self, img, mu = None, sigma = None):
@@ -94,4 +95,32 @@ class dataLoader(object):
             imo = imo[0]
         return imo, mu, sigma        
 
-        
+    def warpImage(self, img):
+        imo = []
+        for i, im in enumerate(img):
+            # add rotation
+            imo += [transform.rotate(im, np.random.rand(1)[0]*(self.rotate_range[1] - self.rotate_range[0])+self.rotate_range[0])]
+            # add translation
+            # add scaling
+            # add bluring
+            # add gaussian noise
+            # cropping
+        return imo
+    
+    def imshow(self, img, tileForShow = False):
+        if tileForShow:
+            mosaic = []
+            n = int(np.sqrt(len(img)))
+            row = []
+            for ii, im in enumerate(img):
+                row += [im]
+                if (ii+1)%n == 0:
+                    mosaic += [np.hstack(row)]
+                    row = []
+            mosaic = np.vstack(row)
+        else: 
+            mosaic = img
+        if mosaic.dtype != np.uint8:
+            L, H = np.min(mosaic), np.max(mosaic)
+            mosaic = (mosaic - L) / (H - L )
+        plt.imshow(mosaic)
