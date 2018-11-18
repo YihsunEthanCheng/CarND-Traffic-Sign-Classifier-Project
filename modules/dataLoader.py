54be84a45467ef898f9e60a5f52c6900a009bf79
Yihsun Ethan Cheng <33135703+YihsunEthanCheng@users.noreply.github.com>
2018-11-17 18:45:44 -0800

    Added batch access to shuffled training set

--- modules/dataLoader.py
+++ modules/dataLoader.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import skimage.transform as skt
 
+
 class dataLoader(object):
     
     def __init__(self, params, path = 'data'):
@@ -19,20 +20,47 @@ class dataLoader(object):
             valid = pickle.load(open(path + '/valid.p','rb'))
         except:
             raise ValueError('Data loading errors..')
-            
+        
+        # update params with data spec
+        self.BATCH_SIZE = params['BATCH_SIZE'] 
         self.nCls = len(np.unique(train['labels']))
         self.imsize = train['features'].shape[1:]
         params.update(self.__dict__)
+        
+        # rename dataset
         self.x_train, self.y_train = train['features'], train['labels']
         self.x_valid, self.y_valid = valid['features'], valid['labels']
-        self.x_test, self.y_test = test['features'], test['labels']
+        self.x_test,  self.y_test  = test['features'],  test['labels']
+        self.labs = np.unique(self.y_valid)
+        self.nPerCls = [ np.sum(self.y_train == lab) for lab in self.labs]
+        self.maxN =  np.max(self.nPerCls)
+        
+        # create lookup table for random sampling for batch training
+        augIdx = []
+        for lab in self.labs:
+            idx_i = np.where(self.y_train == lab)[0] 
+            np.random.shuffle(idx_i)
+            idx_j = idx_i.tolist() * (self.maxN//len(idx_i)+1)
+            augIdx += idx_j[:self.maxN]
+        self.augIdx = np.array(augIdx)
+        np.random.shuffle(self.augIdx)
+        self.augiNext = 0 # circular starting point for the next batch 
         self.showDataStats()
+              
+    def nextBatch(self):
+        ii = self.augIdx[np.arange(self.augiNext, self.augiNext + self.BATCH_SIZE) % len(self.augIdx)]
+        # move pointer 
+        self.augiNext =  (self.augiNext + self.BATCH_SIZE) % len(self.augIdx)
+        if self.augiNext < self.BATCH_SIZE:
+            np.random.shuffle(self.augIdx)
+        return self.x_train[ii], self.y_train[ii]
         
     def showDataStats(self):
         print("Number of training examples =", len(self.y_train))
         print("Number of testing examples =", len(self.y_test))
         print("Image data shape =", self.imsize)
         print("Number of classes =", self.nCls )
+        print("num of Training patterns", self.nPerCls)
         
     def viewSample(self, nPerCls = 20):
         canvas = []
