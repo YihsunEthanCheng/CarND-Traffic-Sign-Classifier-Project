d658a5e514490d8df815c0c5baa3df39e48cfe55
Yihsun Ethan Cheng <33135703+YihsunEthanCheng@users.noreply.github.com>
2018-11-18 21:51:35 -0800

    Added parameters for training data augmentation

--- run_traffic_sign_classifier_LeNet5.py
+++ run_traffic_sign_classifier_LeNet5.py
@@ -8,9 +8,9 @@ Created on Tue Nov 13 23:08:39 2018
 %load_ext autoreload
 %autoreload 2
 import matplotlib.pyplot as plt
-from modules.dataLoader import dataLoader
+from modules.dataLoader import TrafficSignData, scaleForShow, tileForShow, warpImageShow
 from modules.LeNet import LeNet
-import tensorflow as tf
+#import tensorflow as tf
 
 Pool = {
        'ksize': [1, 2, 2, 1],
@@ -25,10 +25,20 @@ C1 = {
 
 C2 = {
        'ksize' : [5, 5],
-       'n': 16,
+       'n': 64,      
        'strides': [1, 1, 1, 1],
        'pool': Pool
        }
+
+# data augmentation parameters
+augment = {
+        'rotate': 15, # +/- degrees
+        'translate': 3, # +/- pixels
+        'scale': 0.1, # 1.0 +/- ratio
+        'Gaussian_blur': 1.2, # Gaussian blur kernel
+        'Gaussian_speckle' : 0.08, # +/- pepper and salt noise
+        'warpFreq': 0.75 # warp the image at 50% of the time
+        }
 
 params = {
         # Arguments used for tf.truncated_normal, randomly defines variables 
@@ -37,21 +47,40 @@ params = {
         'sigma' :  0.1,
         'c1': C1, 
         'c2': C2,
-        'nFC1' : 128,
-        'nFC2' : 64,
+        'nFC1' : 512,
+        'nFC2' : 128,
         'lr' : 0.001,
         'BATCH_SIZE' : 256,
-        'EPOCHS' : 500
+        'EPOCHS' : 200,
+        'param_keep_prob': 0.5,
+        'augment': augment
         } 
+                                                                                                                                                                                                                                                                                    
 
+#%def main():
+data = TrafficSignData(params)
 
-#%%def main():
-  
-data = dataLoader()
-params['nCls'] = data.nCls
-params['imsize'] = data.imsize
+#%%
 model = LeNet(params)
 model.train(data)
+
+#%%
+model.eval(data.x_test, data.y_test)
+#%% dumping for visualizing dataset
+data.viewSample()
+#dump training sample without perturbation
+data.BATCH_SIZE = 16
+img, _ = data.nextBatch()
+plt.figure(1)
+plt.clf()
+imshow(scaleForShow(tileForShow(img)))
+
+#%% show warped images for the same sample
+plt.figure()
+imgWarped = data.warpImage(img)
+imshow(scaleForShow(tileForShow(imgWarped)))
+#%% show steps of image warping for one single image
+warpImageShow(img[9], augment)
 
 
 #%%
